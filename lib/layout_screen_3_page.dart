9417378ccda534d693e822e377e17bceb45e2375
phatdroid94 <phatdroid94@gmail.com>
2024-06-18 18:40:35 +0700

    Group 1

--- lib/layout_screen_3_page.dart
+++ lib/layout_screen_3_page.dart
@@ -16,12 +16,48 @@ class _LayoutScreen3PageState extends State<LayoutScreen3Page> {
       ),
       body: Container(
         constraints: BoxConstraints.expand(),
-        decoration: BoxDecoration(
-          border: Border.all(width: 2)
-        ),
+        decoration: BoxDecoration(border: Border.all(width: 2)),
         child: Column(
           children: [
-            Expanded(flex: 2, child: Container(color: Colors.red)),
+            Expanded(flex: 2,
+                child: Container(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Expanded(
+                            flex: 4,
+                            child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  border: Border(
+                                    right: BorderSide(width: 2),
+                                    bottom: BorderSide(width: 2)
+                                  )
+                                ),
+                                child: const Center(
+                                    child: Text("1", style: TextStyle(color: Colors.white)
+                                    )
+                                )
+                            )
+                        ),
+                        Expanded(flex: 6,
+                            child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.yellow,
+                                    border: Border(
+                                        bottom: BorderSide(width: 2)
+                                    )
+                                ),
+                                child: const Center(
+                                  child: Text("2", style: TextStyle(color: Colors.white)
+                                  )
+                                )
+                            )
+                        )
+                      ]
+                    )
+                )
+            ),
             Expanded(flex: 5, child: Container(color: Colors.green)),
             Expanded(flex: 3, child: Container(color: Colors.blue))
           ],
